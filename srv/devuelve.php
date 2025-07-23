91f60bbdeec7a424446129943ad83878924db314
BugsBustersx0 <bugsbustersxx@gmail.com>
2025-07-23 11:11:29 -0600

    Primer Commit con datos propios

---
+++ srv/devuelve.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once __DIR__ . "/../lib/php/devuelveJson.php";
+
+devuelveJson([
+ "artista" => "Martin Garrix",
+ "Instrumento" => "Tornamesa",
+ "Festival" => "Ultra Miami 2025"
+]);
